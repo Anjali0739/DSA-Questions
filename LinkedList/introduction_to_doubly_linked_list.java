8113795d69e9a3f1c283e4428e64f5384a43c722
Anjali0739 <anjalikumarisoni898@gmail.com>
2025-03-18 23:31:58 +0530

    update

--- LinkedList/introduction_to_doubly_linked_list.java
+++ LinkedList/introduction_to_doubly_linked_list.java
@@ -1,4 +1,17 @@
-
+class Solution {
+    Node constructDLL(int arr[]) {
+        // Code here
+        Node head=new Node(arr[0]);
+        Node curr=head;
+        for(int i=1; i<arr.length; i++){
+            Node node=new Node(arr[i]);
+             node.prev = curr;
+            curr.next = node;
+            curr = node;
+        }
+        return head;
+    }
+}
 
 
 //Question: https://www.geeksforgeeks.org/problems/introduction-to-doubly-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=introduction-to-doubly-linked-list
