70d67721b228abeec708daca1e8a85a0da3c3f19
Anjali0739 <anjalikumarisoni898@gmail.com>
2025-04-13 23:23:39 +0530

    2

---
+++ Binary_Search/search_in_rotated_sorted_array_2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int k=0, n=nums.length;
+        for(int i=1; i<n; i++){
+            if(nums[i-1]>nums[i]) k=i;
+        }
+        for(int i=1; i<n-1; i++){
+            if(nums[i-1]>nums[i] && nums[i]<nums[i+1]) k=i;
+        }
+        int low=0, high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int m=(mid+k)%n;
+            if(nums[m]==target) return true;
+            else if(nums[m]>target) high=mid-1;
+            else low=mid+1;
+        }
+        return false;
+    }
+}
+
+//Question: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
